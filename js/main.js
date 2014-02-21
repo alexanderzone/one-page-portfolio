c94cb118820d8044390aa9c6a730365e7efe60d6
Alexander Mistakidis <mistakidis@gmail.com>
2014-02-20 23:22:21 -0500

    change color of nav to blue; clean up js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,48 @@
 $( document ).ready(function() {
-    $("[rel='tooltip']").tooltip();    
- 
-    // $('.thumbnail').hover(
-    //     function(){
-    //         $(this).find('.caption').slideDown(250); //.fadeIn(250)
-    //     },
-    //     function(){
-    //         $(this).find('.caption').slideUp(450); //.fadeOut(205)
-    //     }
-    // ); 
+  $("[rel='tooltip']").tooltip();
 });
 
 $("#menu-close").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
 });
 $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
 });
 
 $(function() {
-$('a[href*=#]:not([href=#])').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-    || location.hostname == this.hostname) {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+      || location.hostname == this.hostname) {
 
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
-  }
+  });
 });
+
+$(window).scroll(function() {
+  $('.toSlideRight').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+600) {
+      $(this).addClass("slideRight");
+    }
+  });
+  $('.toSlideDown').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+600) {
+      $(this).addClass("slideDown");
+    }
+  });
 });
